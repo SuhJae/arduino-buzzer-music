6b62e7e098a0e40676e5afb4ff708361ea8cb4dd
wonyul <jwsuh25@kis.ac>
2022-10-14 10:37:27 +0900

    Bug fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ mid = mido.MidiFile('music.mid')
 # Edit this value to shift the pitch of the song.
 # 1 means shift one key up, -1 means shift one key down.
 # While 12 means shift one octave up, -12 means shift one octave down.
-key = 12
+key = 13
 
 freq_final = ""
 delay_final = ""
